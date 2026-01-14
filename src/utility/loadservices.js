fc85dd3a0f02a23444c25113b3d0f891cf53f39d
TotoCode <totocodefr@duck.com>
2026-01-14 19:17:54 +0100

    feat(loadservices): only allow service access to /api/service endpoints

--- src/utility/loadservices.js
+++ src/utility/loadservices.js
@@ -3,7 +3,7 @@ import fs from 'node:fs';
 export async function load(expressApp) {
     fs.readdirSync('./src/services').forEach(async (file) => {
         const { default: { routers, middlewares } } = await import(`../services/${file}/main.js`);
-        routers.forEach(router => expressApp.use(router));
-        middlewares.forEach(middleware => expressApp.use(middleware));
+        routers.forEach(router => expressApp.use(`/api/${file}`, router));
+        middlewares.forEach(middleware => expressApp.use(`/api/${file}`, middleware));
     });
 }
