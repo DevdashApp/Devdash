3619e2b104ca1a6877c0f583535778e881dc893f
TotoCodeFR <totocodefr@duck.com>
2026-01-15 13:51:56 +0100

    started dynamic service page loading, but i'm sadly having a problem with file serving...

---
+++ src/app/script.js
@@ -0,0 +1,49 @@
+function flushJSandCSS() {
+    Array.from(document.getElementsByClassName('service-specific')).forEach((element) => {
+        element.remove();
+    });
+}
+
+function loadContent() {
+    const service = window.location.href.split('/')[4];
+    console.log(service);
+    if (service) {
+        flushJSandCSS();
+        fetch("/services/content/" + service + "/index.html")
+            .then(response => response.text())
+            .then(html => {
+                const contentDiv = document.getElementById('content');
+
+                const scriptElements = contentDiv.querySelectorAll('script');
+                scriptElements.forEach(script => {
+                    const newScript = document.createElement('script');
+                    if (script.src) {
+                        newScript.src = '/services/content/' + script.src.split('/').pop();
+                    } else {
+                        newScript.textContent = script.textContent;
+                    }
+                    contentDiv.appendChild(newScript);
+                    script.classList.add('service-specific');
+                    script.remove(); // nettoyer le script inactif
+                });
+
+                const linkElements = contentDiv.querySelectorAll('link[rel="stylesheet"]');
+                linkElements.forEach(link => {
+                    const newLink = document.createElement('link');
+                    newLink.rel = 'stylesheet';
+                    newLink.href = '/services/content/' + service + link.href.split('/').pop();
+                    newLink.crossOrigin = 'anonymous';
+                    newLink.type = 'text/css';
+                    newLink.classList.add('service-specific');
+                    document.head.appendChild(newLink);
+                    link.remove();
+                });
+            })
+            .catch(error => {
+                console.error('Erreur lors du chargement du contenu :', error);
+            });
+
+    }
+}
+
+loadContent();
--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static('src/public'));
 
-app.use('/app', express.static('src/app'));
+app.use('/app*splat', express.static('src/app'));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
