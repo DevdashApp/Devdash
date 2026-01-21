1ad2bf2e2d84491e6e83e5965c620a940e5e518c
TotoCode <totocodefr@duck.com>
2026-01-21 17:56:13 +0100

    chore: removed useless logs

--- src/utility/loadservices.js
+++ src/utility/loadservices.js
@@ -3,9 +3,9 @@ import fs from 'node:fs';
 export async function load(expressApp) {
     fs.readdirSync('./src/services').forEach(async (file) => {
         const { default: { routers, middlewares, frontendRouters } } = await import(`../services/${file}/main.js`);
+
         routers.forEach(router => expressApp.use(`/api/${file}`, router));
         middlewares.forEach(middleware => expressApp.use(`/api/${file}`, middleware));
         frontendRouters.forEach(router => expressApp.use(`/services/${file}/`, router));
-        console.log(`/services/${file}`);
     });
 }
