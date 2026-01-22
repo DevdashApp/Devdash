78d5251cd464579cc5fff11fcdfd3369f8788bf1
TotoCodeFR <totocodefr@duck.com>
2026-01-22 14:07:31 +0100

    added gh api access through octokit

--- src/services/github/main.js
+++ src/services/github/main.js
@@ -1,8 +1,9 @@
-import { App, createNodeMiddleware } from "octokit";
+import { App, createNodeMiddleware, Octokit } from "octokit";
 import fs from "node:fs/promises";
 import { Router, static as static_ } from "express";
+import path from "node:path";
 
-const githubApp = new App({
+const app = new App({
     appId: process.env.GITHUB_APP_ID,
     privateKey: await fs.readFile("devdash-handler.pem", "utf-8"),
     oauth: {
@@ -13,42 +14,62 @@ const githubApp = new App({
     webhooks: {
         secret: process.env.GITHUB_WEBHOOK_SECRET,
         path: "/webhooks",
-    }
+    },
 });
+
+const octokit = new Octokit();
 
 const router = new Router();
-const frontendRouter = new Router();
 
-githubApp.oauth.on("token.created", async ({ token, octokit }) => {
-    console.log("New user token created!");
-
-    const { data: user } = await octokit.rest.users.getAuthenticated();
-    console.log("User login:", user.login);
+router.get("/oauth/login", async (_req, res) => {
+    res.redirect(
+        await app.getInstallationUrl({
+            state: "random_csrf_token",
+        })
+    );
 });
 
-console.log("Install URL:", await githubApp.getInstallationUrl({
-    state: "random_csrf_token",
-}));
-
-router.get('/oauth/login', async (req, res) => {
-    res.redirect(await githubApp.getInstallationUrl({
-        state: 'random_csrf_token'
-    }));
-});
-
-router.get('/oauth/callback', async (req, res) => {
+router.get("/oauth/callback", async (req, res) => {
     const code = req.query.code;
     if (!code) return res.status(400).send("Missing code");
 
-    await githubApp.oauth.createToken({ code });
-
-    res.redirect("/app/");
+    await app.oauth.createToken({ code });
+    res.redirect("/app/github");
 });
 
-frontendRouter.use('/', static_('src/services/github/frontend/'));
+app.oauth.on("token.created", async ({ token, app }) => {
+    console.log("New user token created!");
+
+    const { data: user } = await app.rest.users.getAuthenticated();
+    console.log("User login:", user.login);
+});
+
+router.get("/profile/get", async (req, res) => {
+    try {
+        const { data } = await octokit.request(
+            `GET /users/${req.query.username}`
+        );
+        res.json(data);
+    } catch (err) {
+        if (err.status == 404) {
+            res.status(404).json({ error: "User not found" });
+        } else {
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+});
+
+const frontendRouter = new Router();
+
+frontendRouter.get("/:username", (req, res) => {
+    res.sendFile(path.resolve('src/services/github/frontend/profile/index.html'));
+});
+
+frontendRouter.get('/:username/script.js', (req, res) => res.sendFile(path.resolve('src/services/github/frontend/profile/script.js')));
+frontendRouter.get('/:username/style.css', (req, res) => res.sendFile(path.resolve('src/services/github/frontend/profile/style.css')));
 
 export default {
     routers: [router],
-    middlewares: [createNodeMiddleware(githubApp.webhooks)],
-    frontendRouters: [frontendRouter]
+    middlewares: [createNodeMiddleware(app.webhooks)],
+    frontendRouters: [frontendRouter],
 };
