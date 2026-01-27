483c425cbb6d31d58b4fdbc308439a8ad7f3911a
TotoCode <totocodefr@duck.com>
2026-01-27 17:06:12 +0100

    fix unlink script

--- scripts/lib-unlink.js
+++ scripts/lib-unlink.js
@@ -5,7 +5,7 @@ const libPath = path.join(process.cwd(), 'src', 'library');
 
 try {
     execSync('npm unlink @devdash/library', { stdio: 'inherit' });
-    execSync('npm unlink', { cwd: libPath, stdio: 'inherit' });
+    execSync('npm un -g @devdash/library', { cwd: libPath, stdio: 'inherit' });
 } catch (error) {
     console.error('Error:', error.message);
     process.exit(1);
